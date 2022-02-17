ce2b8ae1698b9d517b8dd8aa851d86a7acf19f4b
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-02-17 23:08:59 +0600

    last touch

--- js/main.js
+++ js/main.js
@@ -6,21 +6,21 @@ function expenseTotal(){
      const foodCost = parseFloat(food);
      if (isNaN(foodCost) || foodCost < 0) {
         food.value = ''
-        return alert('Please input valid amount of money in number format')
+        return alert('Please input valid amount of money in food amount box')
       }
      const rent =document.getElementById('rent').value;
      const rentCost =parseFloat(rent);
      if (isNaN(rentCost) || rentCost < 0) {
         rent.value = ''
-        return alert('Please input valid amount of money in number format')
+        return alert('Please input valid amount of money in rent amount box')
       }
      const clothes =document.getElementById('clothes').value;
      const clothCost = parseFloat(clothes);
      if (isNaN(clothCost) || clothCost < 0) {
         clothes.value = ''
-        return alert('Please input valid amount of money in number format')
+        return alert('Please input valid amount of money in clothes amount box')
       }
-     const totalExpense = foodCost + rentCost +clotheCost;
+     const totalExpense = foodCost + rentCost +clothCost;
      document.getElementById('total-expense').innerText =totalExpense;
     
 return totalExpense}
@@ -32,8 +32,13 @@ return totalExpense}
        const income = parseFloat(incomeInput);
        if (isNaN(income) || income < 0) {
         incomeInput.value = ''
-        return alert('Please input valid amount of money in number format')
+        return alert('Please input valid amount of money in income amount box')
       }
+     
+      if (isNaN(income) || income < totalExpense) {
+       
+        return alert('Please enter your correct expense')
+      }  
        const balance = income - totalExpense;
        document.getElementById('balance').innerText= balance;
        return income, balance;
@@ -44,13 +49,18 @@ return totalExpense}
      const save = parseFloat(saveInput);
      if (isNaN(save) || save < 0) {
         saveInput.value = ''
-        return alert('Please input valid amount of money in number format')
+        return alert('Please input valid amount of money in save amount box')
       }
      const incomeTotal =income.value;
      const saveRatefirst = save/100;
      const saveRate = parseFloat(saveRatefirst)
      const balanceTotal =balance.innerText;
      const saveAmount =incomeTotal * saveRate;
+     if (isNaN(balanceTotal) || balanceTotal < saveAmount) {
+       
+      return alert('Please enter your correct saving amount')
+    }  
+    
      const remainingBalance = balanceTotal - saveAmount 
      document.getElementById('saving-amount').innerText=saveAmount;
      
@@ -59,6 +69,7 @@ return totalExpense}
     } 
 document.getElementById('calculate').addEventListener('click', function(){
  balanceTotal();
+
 });
 document.getElementById('save-btn').addEventListener('click', function(){
     saveTotal();
