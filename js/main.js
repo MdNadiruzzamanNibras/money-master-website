49c7b3eaf9da0d3ab56d1349fa1fb46dc8920b23
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-02-17 10:56:25 +0600

    totalExpense

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+function expensive(){
+    const food =document.getElementById('food').value;
+     const foodCost = parseFloat(food);
+     const rent =document.getElementById('rent').value;
+     const rentCost =parseFloat(rent);
+     const clothes =document.getElementById('clothes').value;
+     const clotheCost = parseFloat(clothes);
+     const totalExpense = foodCost + rentCost +clotheCost;
+     document.getElementById('total-expense').innerText =totalExpense;
+     return totalExpense;
+}
+
 document.getElementById('calculate').addEventListener('click', function(){
-    console.log('click')
+ expensive()
 })
